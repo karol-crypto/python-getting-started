3994f23cf6b67a011f65310d62749978c61c3768
karol-crypto <karolwojnarowski.21@gmail.com>
2026-01-04 17:28:56 +0100

    fix allowed hosts

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -50,13 +50,12 @@ DEBUG='TRUE'
 # also explicitly exclude CI:
 # https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
 IS_HEROKU_APP = "DYNO" in os.environ and "CI" not in os.environ
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lab1-python-i3gm.onrender.com"]
 if IS_HEROKU_APP:
     # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS`, since the Heroku router performs
     # validation of the Host header in the incoming HTTP request. On other platforms you may need to
     # list the expected hostnames explicitly in production to prevent HTTP Host header attacks. See:
     # https://docs.djangoproject.com/en/6.0/ref/settings/#std-setting-ALLOWED_HOSTS
-    #ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lab1-python-i3gm.onrender.com"]
 
     # Redirect all non-HTTPS requests to HTTPS. This requires that:
     # 1. Your app has a TLS/SSL certificate, which all `*.herokuapp.com` domains do by default.
@@ -68,7 +67,7 @@ if IS_HEROKU_APP:
     #
     # For maximum security, consider enabling HTTP Strict Transport Security (HSTS) headers too:
     # https://docs.djangoproject.com/en/6.0/ref/middleware/#http-strict-transport-security
-    SECURE_SSL_REDIRECT = TRUE
+    SECURE_SSL_REDIRECT = True
 else:
     ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0", "[::]"]
 
