c8a15ce34217d5c56bee471286ec5e6d5dca185c
karol-crypto <karolwojnarowski.21@gmail.com>
2026-01-04 18:35:29 +0100

    fix dis hosts

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -50,12 +50,9 @@ DEBUG=True
 # also explicitly exclude CI:
 # https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
 IS_HEROKU_APP = "DYNO" in os.environ and "CI" not in os.environ
-# Zawsze bazowa lista hostów (lokalnie + Render)
+
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lab1-python-i3gm.onrender.com"]
-if os.environ.get('IS_HEROKU_APP') == '1':
-    ALLOWED_HOSTS.append('*')
-if os.environ.get('ALLOWED_HOSTS'):
-    ALLOWED_HOSTS.extend(os.environ.get('ALLOWED_HOSTS').split(','))
+
 if IS_HEROKU_APP:
     # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS`, since the Heroku router performs
     # validation of the Host header in the incoming HTTP request. On other platforms you may need to
@@ -74,7 +71,11 @@ if IS_HEROKU_APP:
     # For maximum security, consider enabling HTTP Strict Transport Security (HSTS) headers too:
     # https://docs.djangoproject.com/en/6.0/ref/middleware/#http-strict-transport-security
     SECURE_SSL_REDIRECT = True
+else:
+    SECURE_SSL_REDIRECT = False
 
+if os.environ.get('ALLOWED_HOSTS'):
+    ALLOWED_HOSTS.extend(os.environ.get('ALLOWED_HOSTS').split(','))
 # Application definition
 
 # Several optional Django features that are present in the default `startproject` template have
